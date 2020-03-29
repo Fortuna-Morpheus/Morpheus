d867eda38c394389df0a7aafb111e833176f36bd
Ted Zamarron <ted.zamarron@gmail.com>
2020-03-29 17:12:26 -0500

    modified JSON and JS. added simple table

--- src/index.js
+++ src/index.js
@@ -5,28 +5,24 @@ $(document).ready(function () {
 
     const {getBills} = require('./api.js');
 
+    // Function to filter bills according to requested type (Bi-Weekly, Monthly, Yearly)
+    let getBillsMonthly = freqType => {
+        let requestedBills = []; // Empty bucket for the monthly bills
+        getBills().then(data => { // Retrieve bills JSON then do something with the data
+            // console.log(data) // Console log for debugging
+            data.forEach(bill => { // Cycle thru each bill
+                if (bill.frequency === freqType) { // if the bill's frequency is equal to type requested
+                    requestedBills.push(bill); // Push bill into monthly bill array
+                }
+            });
+        });
+        return requestedBills; // returns array of bills meeting freq requested
+    };
 
-    function getBillsMonthly() {
-        getBills().then(function (data) {
-            data.forEach(function (expenses) {
-                expenses.monthly.forEach( function (bills) {
-                    console.log(bills);
-                })
-            })
-        })
-    }
-    getBillsMonthly();
 
-    function getBillsYearly() {
-        getBills().then(function (data) {
-            data.forEach(function (expenses) {
-                expenses.yearly.forEach( function (bills) {
-                    console.log(bills);
-                })
-            })
-        })
-    }
-    getBillsYearly();
+    console.log(getBillsMonthly("Bi-Weekly"));
+    console.log(getBillsMonthly("Monthly"));
+    console.log(getBillsMonthly("Yearly"));
 
 
 });
